384a9efbd08c2bd9c7fabe08e7a0c31afcd0d259
Celemm <ngolaceleste@gmail.com>
2025-05-07 11:05:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulaire admin</title>
+</head>
+<body>
+    <header>
+        <h1>Formulaire Admin</h1>
+    </header>
+    <main>
+        <footer>
+            <p>
+                &copy; - PHP - 2025
+            </p>
+        </footer>
+    
+</body>
+</html>
